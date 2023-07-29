6fcf11ec6c690735852174c7c12588b108e62b40
MarFueGit <maricelafuentes100@gmail.com>
2023-07-29 17:16:57 -0600

    add:se agrego modal de detalle

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ const contenedorPersonajes = document.querySelector(".contenedor-card");
 const buscador = document.querySelector(
   "input[placeholder='Escribe un nombre']"
 );
+const modalPersonaje = document.querySelector(".modal-personaje");
 
-renderizarPersonajes(personajes, contenedorPersonajes);
+renderizarPersonajes(personajes, contenedorPersonajes, modalPersonaje);
 //Listener
 buscador.addEventListener("keyup", (e) => {
   // console.log(e.target.value);
-
   const resultados = personajes.filter(
     (personaje) =>
       personaje.nickname.toLowerCase().includes(e.target.value) ||
@@ -25,6 +25,26 @@ buscador.addEventListener("keyup", (e) => {
     contenedorPersonajes.removeChild(contenedorPersonajes.firstChild);
   }
 
-  renderizarPersonajes(resultados, contenedorPersonajes);
+  renderizarPersonajes(resultados, contenedorPersonajes, modalPersonaje);
 });
 //Logic
+
+//Codigo modal
+// const cerrar = document.querySelectorAll(".close")[0];
+// const abrir = document.querySelectorAll(".open")[0];
+// const modal = document.querySelectorAll(".modal")[0];
+// const modalPersonaje = document.querySelectorAll(".modal-personaje")[0];
+
+// abrir.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   modalPersonaje.style.opacity = "1";
+//   modalPersonaje.style.visibility = "visible";
+//   modal.classList.toggle("modal-close");
+// });
+
+// cerrar.addEventListener("click", function () {
+//   modal.classList.toggle("modal-close");
+//   modalPersonaje.style.opacity = "0";
+//   modalPersonaje.style.visibility("hidden");
+//   setTimeout(function () {}, 1000);
+// });
