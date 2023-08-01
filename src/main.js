87e33cd092d982f038841b037ccb6e921180ee3f
MarFueGit <maricelafuentes100@gmail.com>
2023-07-31 20:17:43 -0600

    add: se agregaron filtros

--- src/main.js
+++ src/main.js
@@ -1,50 +1,59 @@
 // Importamos la data del archivo de Breakingbad.
 import data from "./data/breakingbad/breakingbad.js";
-import { renderizarPersonajes } from "./function.js";
+import { renderizarPersonajes } from "./data.js";
+
 // Guardamos el array de personajes en una variable
 const personajes = data.breaking_bad;
 //Referencias DOM
-
 const contenedorPersonajes = document.querySelector(".contenedor-card");
 const buscador = document.querySelector(
   "input[placeholder='Escribe un nombre']"
 );
 const modalPersonaje = document.querySelector(".modal-personaje");
 
+// Guardamos el id  del select en una variable
+const categoria = document.getElementById("categoria");
+const temporadas = document.getElementById("temporadas");
+const ordenamiento = document.getElementById("ordenamiento");
+
 renderizarPersonajes(personajes, contenedorPersonajes, modalPersonaje);
 //Listener
 buscador.addEventListener("keyup", (e) => {
-  // console.log(e.target.value);
   const resultados = personajes.filter(
     (personaje) =>
       personaje.nickname.toLowerCase().includes(e.target.value) ||
       personaje.name.toLowerCase().includes(e.target.value)
   );
 
-  while (contenedorPersonajes.firstChild) {
-    contenedorPersonajes.removeChild(contenedorPersonajes.firstChild);
-  }
-
+  // Invocamos la funcion que renderiza a los personajes de acuerdo al array filtrado
   renderizarPersonajes(resultados, contenedorPersonajes, modalPersonaje);
 });
-//Logic
 
-//Codigo modal
-// const cerrar = document.querySelectorAll(".close")[0];
-// const abrir = document.querySelectorAll(".open")[0];
-// const modal = document.querySelectorAll(".modal")[0];
-// const modalPersonaje = document.querySelectorAll(".modal-personaje")[0];
+// Listener del select categoria que se invoca al seleccionar una categoria(change)
+categoria.addEventListener("change", (e) => {
+  const resultados = personajes.filter((personaje) =>
+    personaje.category.includes(e.target.value)
+  );
+  renderizarPersonajes(resultados, contenedorPersonajes, modalPersonaje);
+});
 
-// abrir.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   modalPersonaje.style.opacity = "1";
-//   modalPersonaje.style.visibility = "visible";
-//   modal.classList.toggle("modal-close");
-// });
+// Listener del select temporadas
+temporadas.addEventListener("change", (e) => {
+  const resultados = personajes.filter((personaje) =>
+    personaje.appearance.includes(Number(e.target.value))
+  );
+  renderizarPersonajes(resultados, contenedorPersonajes, modalPersonaje);
+});
 
-// cerrar.addEventListener("click", function () {
-//   modal.classList.toggle("modal-close");
-//   modalPersonaje.style.opacity = "0";
-//   modalPersonaje.style.visibility("hidden");
-//   setTimeout(function () {}, 1000);
-// });
+// Listener del select ascendente y descendente
+ordenamiento.addEventListener("change", (e) => {
+  let resultados = [];
+  if (e.target.value === "ascedente") {
+    // Esto es ordenar de forma ascendente (a - z)
+    resultados = personajes.sort((a, b) => a.name.localeCompare(b.name));
+  } else {
+    // Esto es ordenar de forma descendente (z - a)
+    resultados = personajes.sort((a, b) => b.name.localeCompare(a.name));
+  }
+  renderizarPersonajes(resultados, contenedorPersonajes, modalPersonaje);
+});
